699ada738f367e2d27826b4e7a94fa9757209ff3
Carlos Rodas <rodasrene939@gmail.com>
2022-07-01 22:40:54 -0600

    proyecto-acordeon-de-contenido
    Proyecto que muestra el contenido simulando un acordeon

---
+++ acordeon-de-contenido/main.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll('.acordeon .item .header button');
+
+items.forEach(item => {
+    item.addEventListener('click', e => {
+        const content = e.target.parentElement.nextElementSibling;
+        content.classList.toggle('show');
+    })
+})
